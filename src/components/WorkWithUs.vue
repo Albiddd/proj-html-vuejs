c671e74a5d5a57b9778abd90bfc05d4c0b525b2c
Albiddd <alberto.dore93@gmail.com>
2022-09-15 10:51:34 +0200

    add work with us section

--- src/components/WorkWithUs.vue
+++ src/components/WorkWithUs.vue
@@ -1,19 +1,65 @@
 <template>
-    <div class="">
-   
+  <div class="section-bg">
+    <div class="container">
+      <div class="title">
+        Want to work with us? Send us
+        <strong>a message!</strong>
+      </div>
+      <p class="text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore. Ut enim ad minim veniam, quis
+        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commo.
+      </p>
+      <button class="btn">contact</button>
     </div>
-  </template>
+  </div>
+</template>
   
   <script>
-  export default {
-    name: 'WorkWithUs',
-    props: {
-  
-    }
-  }
-  </script>
+export default {
+  name: "WorkWithUs",
+  props: {},
+};
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped lang="scss">
-  
-  </style>
+.section-bg {
+  background-image: url("https://maree.qodeinteractive.com/wp-content/uploads/2019/05/h-2-background-1.jpg?id=589");
+  background-repeat: no-repeat;
+  background-size: cover;
+  // height: 529px;
+  // margin-top: 180px;
+}
+
+.container {
+  padding: 114px 16% 119px 16%;
+  text-align: center;
+  .title {
+    font-size: 65px;
+    line-height: 1.261em;
+    strong {
+      font-style: italic;
+      font-family: "Playfair Display", serif;
+    }
+  }
+  .text {
+    color: rgba(0, 0, 0, 0.651);
+    font-size: 20px;
+    line-height: 30px;
+    font-weight: 300;
+    margin-top: 23px;
+    margin-block: 37px;
+  }
+  .btn{
+    font-size: 13px;
+    line-height: 1.8em;
+    letter-spacing: .25em;
+    font-weight: 700;
+    text-transform: uppercase;
+    background-color: #D8F0E3;
+    padding: 17px 70px;
+    border: none;
+  }
+}
+</style>
