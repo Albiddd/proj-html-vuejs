0eb5c34b019b1aa36ca48cf14b417600376e8deb
Albiddd <alberto.dore93@gmail.com>
2022-09-16 13:49:38 +0200

    add navbar hover

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,12 @@
         <ul class="nav">
             <li v-for="(element, i) in menu" 
             :key="i" class="menu-link">
-            <a href="#">{{element}}</a>
+            <a href="#">
+                <span>
+                    {{element}}
+                    <div :class="`nav-el-${i}`"></div>
+                </span>
+            </a>
             </li>
             <!-- <ul class="dropdown">
                 <li v-for="(el, i) in drops" :key="i" class="dropdown-el">{{el}}</li>
@@ -53,6 +58,41 @@
                 text-transform: uppercase;
                 color: #000;
                 line-height: 14px;
+                span{
+                    position: relative;
+                    isolation: isolate;
+                    div{
+                        width: 0;
+                        height: 6px;
+                        z-index: -1;                       
+                        display: block;
+                        transition: width 0.2s;
+                        position: absolute;
+                        bottom: 2px;
+                        left: 0;
+                    }
+                    &:hover div{
+                        width: 100%;
+                    }
+                    &:hover .nav-el-0{
+                        background-color: #F0CEBE;
+                    }
+                    &:hover .nav-el-1{
+                        background-color: #E6E2D1;
+                    }
+                    &:hover .nav-el-2{
+                        background-color: #D8F0E3;
+                    }
+                    &:hover .nav-el-3{
+                        background-color: #EBE8F9;
+                    }
+                    &:hover .nav-el-4{
+                        background-color: #FEF2BD;
+                    }
+                    &:hover .nav-el-5{
+                        background-color: #FFD9B4;
+                    }
+                }
             }
         }
     }
